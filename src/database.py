297501c9b9173f6b86ae7895d163c10371425e4e
Gabriel <gabrielnetbrine@gmail.com>
2023-10-17 17:30:47 -0300

    :sparkles: feat: acesso ao banco de dados
    - inserção
    - remoção

---
+++ src/database.py
@@ -0,0 +1,64 @@
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
+from datetime import *
+import main
+
+class DataBase:
+
+    def __init__(self): #construtor
+        
+        load_dotenv()
+        self.offers = self.connect()
+
+    def connect(self): #conectando ao banco de dados
+
+        client = MongoClient(os.getenv('DB_URI'))
+        datab = client['curso']
+        return datab.offers 
+    
+    def insert(self,data: dict): #insercao 
+        
+        query = {'datenow' : data['datenow']}
+        result = self.offers.find_one(query)
+
+        if result is not None:
+            print("Dados ja adicionados")
+        else:
+    
+            self.offers.insert_one(data) #INSERIR FILTRO
+            print("Dados inseridos")
+
+    def find(self,query: dict): #busca
+
+        result = self.offers.find_one(query)
+
+        if result is not None:
+            print("Dado encontrado")
+            return result
+            
+        else:
+            print("Dado nao encontrado")
+            
+
+if __name__ == "__main__":
+
+    obj = DataBase() 
+    objMain = main.Crawler()
+
+    data = objMain.extractNecessary()
+    dateNow = str(datetime.now().date())
+
+    # inserindo
+    obj.insert(data)
+
+    # buscando
+    result = obj.find({'datenow' : dateNow})
+    print(result)
+
+    
+
+    
+
+
+    
